6ccb22325d048f98baaaf5c9f070faff375038f1
Muhamed <imamovic.net@gmail.com>
2025-04-16 22:37:40 +0200

    more functionality

--- Encapsulation_test/src/Account.java
+++ Encapsulation_test/src/Account.java
@@ -1,9 +1,11 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.UUID;
 
 public class Account {
 
+    private String accNumber;
     Scanner input = new Scanner(System.in);
     private double balance;
     private String accountType;
@@ -11,13 +13,45 @@ public class Account {
     private ArrayList<String> transactionHistory = new ArrayList<>();
 
     public Account(double balance, String accountType, String accName){
-        if (balance < 250){
-            System.out.println("Minimum account balance is $250");
+        if (balance < 0){
+            System.out.println("Minimum account balance is $0");
         }else {
             this.balance = balance;
         }
         this.accountType = accountType;
         this.accName = accName;
+        this.accNumber = loadAccNumber();//generateAccNumber();
+        if (loadAccNumber() == null){
+            this.accNumber = generateAccNumber();
+            saveAccNumber(this.accNumber);
+        }
+    }
+
+    private String loadAccNumber(){
+        File file = new File("account_number.txt");
+
+        if (!file.exists()){
+            return null;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            return reader.readLine();
+        }catch (IOException e){
+            System.out.println("Error reading account number " + e.getMessage());
+            return null;
+        }
+    }
+
+    private String generateAccNumber(){
+        return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 12).toUpperCase();
+    }
+
+    private void saveAccNumber(String number){
+        try (FileWriter writer = new FileWriter("account_number.txt")) {
+            writer.write(number);
+        }catch (IOException e) {
+            System.out.println("Error saving account number" + e.getMessage() + ".");
+        }
     }
 
     private double calculateInterest() {
@@ -45,19 +79,22 @@ public class Account {
         double overAmount = 0;
         if (this.balance - amount < -1000){
             remaining = -1000 + this.balance;
-            System.out.printf("Account balance is %s.%n Maximum overdraft amount is %s.", this.balance, remaining);
-            //dodat ovdje jos jedan if else
+            System.out.printf("Insufficient funds.%nAccount balance is %.2f.%nYour maximum overdraft amount is %s.", this.balance, remaining);
             transactionHistory.add("Overdraft withdrawal failed, not enough funds");
         }else {
             overAmount = amount - this.balance;
             this.balance -= amount;
-            System.out.println("Current account balance is " + this.balance);
+            System.out.printf("Overdraft successful.%nCurrent account balance is " + this.balance + "%n");
             transactionHistory.add("Successful overdraft of " + overAmount);
         }
     }
 
     public void getHistory(){
         System.out.printf("Your transaction history: %s%n", this.transactionHistory);
+    }
+
+    public void getAccNumber(){
+        System.out.println(this.accNumber);
     }
 
     public void setAccountType(String newType){
@@ -87,15 +124,19 @@ public class Account {
 
     public void withdraw(double amount){
         if (this.balance - amount < 0){
-            System.out.printf("You cannot withdraw that amount. Your current account balance is %.2f./nMinimum account balance is $0.%n", this.balance);
+            System.out.printf("You cannot withdraw that amount. Your current account balance is %.2f.%nMinimum account balance is $0.%n", this.balance);
             System.out.printf("Would you like to overdraft? Yes/No");
             //dodat provjeru jel vec na max overdraft
             String overdraftAnsw = input.nextLine();
-            if (overdraftAnsw.equals("No")) {
+            if (overdraftAnsw.equals("Yes") && this.balance == -1000){
+                System.out.printf("Insufficient funds./nMaximum overdraft is $-1000.%nYour account balance is: $.2f.%n", this.balance);
+                transactionHistory.add("Failed withdrawal of " + amount + ", Insufficient funds.");
+            }else if (overdraftAnsw.equals("No")) {
                 System.out.println("Thank you for using our services.");
                 transactionHistory.add("Failed withdrawal of " + amount);
             }else{
                 overdraft(amount);
+                return;
             }
         }else {
             this.balance -= amount;
--- Encapsulation_test/src/Main.java
+++ Encapsulation_test/src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Account myAccount = new Account(1000, "Savings", "Medo");
-        Account myAccount2 = new Account(2500, "Checkings", "Sara");
+        Account myAccount = new Account(0, "Savings", "Medo");
 
-        myAccount.getBalance();
-        myAccount.getHistory();
-        myAccount.withdraw(1200);
-        myAccount.getHistory();
     }
 }
